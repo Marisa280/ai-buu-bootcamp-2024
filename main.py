3473c874b533495cc59230216c750c7a8633c2ea
Marisa280 <153882689+Marisa280@users.noreply.github.com>
2024-12-12 13:40:44 +0700

    main.py
    update api

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ ACCESS_TOKEN = "example"
 CHANNEL_SECRET = "example"
 
 # ข้อมูล Gemini api key
-GEMINI_API_KEY = "example"
+GEMINI_API_KEY = "AIzaSyB4ifa1wad825MlwFWuogtll2_sEbxSjlk"
 
 # การเชื่อมต่อ และตั้งค่าข้อมูลเพื่อเรียกใช้งาน LINE Messaging API
 configuration = Configuration(access_token=ACCESS_TOKEN)
